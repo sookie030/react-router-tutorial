25d44280572e75af1c5ee07b7c98efb51570b184
sookie030 <mschoi0122@gmail.com>
2020-04-28 20:05:06 +0900

    scanner 모듈 추가

--- src/components/nodeProperty/AIProperty.jsx
+++ src/components/nodeProperty/AIProperty.jsx
@@ -15,4 +15,11 @@ property[MODULES.DECISION_MAKER] = node => {
   return [`Category ${node.getProperties().getIn(['Category', 'value'])}`];
 };
 
+property[MODULES.SCANNER] = node => {
+  return [
+    `ROI ${node.getProperties().getIn(['ROI', 'properties', 'Width', 'value'])}x${node.getProperties().getIn(['ROI', 'properties', 'Height', 'value'])}`,
+    `Stride ${node.getProperties().getIn(['Stride', 'properties', 'x', 'value'])}, ${node.getProperties().getIn(['Stride', 'properties', 'y', 'value'])}`,
+  ];
+};
+
 export default property;
--- src/manager/modules/AI.js
+++ src/manager/modules/AI.js
@@ -11,6 +11,9 @@ import { MODULES } from "../../constants/ModuleInfo";
 import ModuleDataChunk from "./ModuleDataChunk";
 import ModuleData from "./ModuleData";
 
+// import utils
+import * as ImageFormatConverter from "../../utils/ImageFormatConverter";
+
 const nmengine = require("../../lib/nmengine/corewrap");
 const constants = require("../../lib/nmengine/constants");
 const ArrayType = window.ArrayType;
@@ -123,10 +126,11 @@ ai[MODULES.NM500] = class extends ModuleBase {
 
     this.connect(0);
     this.setContext();
-    this.learnTest(10, 3, 1, 1);
-    this.learnTest(20, 3, 2, 1);
-    this.learnTest(50, 3, 5, 1);
-    this.modelStatTest();
+    // this.learnTest(10, 3, 1, 1);
+    // this.learnTest(20, 3, 2, 1);
+    // this.learnTest(50, 3, 5, 1);
+    // this.classifyTest(10, 3, 3);
+    // this.modelStatTest();
   }
 
   /**
@@ -230,57 +234,100 @@ ai[MODULES.NM500] = class extends ModuleBase {
   };
 
   learnTest = (data, size, category, queryAffected) => {
-
     let vector = Array(constants.NEURON_MEMORY).fill(0);
 
     // Make input vector
-    console.log("[tcLearn] VECTOR: ")
+    console.log("[tcLearn] VECTOR: ");
     for (let i = 0; i < size; i++) {
-        vector[i] = data;
-        console.log(" " + data)
+      vector[i] = data;
+      console.log(" " + data);
     }
-    console.log(", CAT: " + category)
+    console.log(", CAT: " + category);
 
     // Training
     let learn = nmengine.learn(vector, category, queryAffected);
 
     // Training result
     if (learn.resultCode === constants.SUCCESS) {
-        let req = learn.req;
-        console.log(",\tRESULT: %d\n", req.status);
+      let req = learn.req;
+      console.log(",\tRESULT: %d\n", req.status);
 
-        if (queryAffected === 1) {
-            for (let i = 0; i < req.affected_count; i++) {
-                console.log("affected neuron nid: %d, cat: %d, aif: %d\n", req.affected_neurons[i].nid, req.affected_neurons[i].cat, req.affected_neurons[i].aif);
-            }
+      if (queryAffected === 1) {
+        for (let i = 0; i < req.affected_count; i++) {
+          console.log(
+            "affected neuron nid: %d, cat: %d, aif: %d\n",
+            req.affected_neurons[i].nid,
+            req.affected_neurons[i].cat,
+            req.affected_neurons[i].aif
+          );
         }
+      }
     } else {
-        console.log("[tcLearn] Error: Failed to learn. %d\n", learn.resultCode);
+      console.log("[tcLearn] Error: Failed to learn. %d\n", learn.resultCode);
     }
-  }
+  };
+
+  classifyTest = (data, size, k) => {
+    let vector = Array(constants.NEURON_MEMORY).fill(0);
+
+    // Make input vector
+    console.log("[tcClassify] VECTOR: ");
+    for (let i = 0; i < size; i++) {
+      vector[i] = data;
+      console.log(" " + data);
+    }
+    console.log(", k: " + k + "\n");
+    console.log(vector);
+    let classify = nmengine.classify(k, vector);
+
+    // Classify result
+    if (classify.resultCode === constants.SUCCESS) {
+      let req = classify.req;
+
+      for (let i = 0; i < req.matched_count; i++) {
+        console.log(
+          "Matched[%d] NID: %d DISTANCE: %d CAT: %d",
+          i,
+          req.nid[i],
+          req.distance[i],
+          req.category[i]
+        );
+      }
+
+      console.log("NETWORK STATUS: %d\n", req.status);
+    } else {
+      console.log(
+        "[tcClassify] Error: Failed to classify. %d\n",
+        classify.resultCode
+      );
+    }
+  };
 
   modelStatTest = () => {
-
-    let uint16Array = ArrayType('uint16');
+    let uint16Array = ArrayType("uint16");
 
     let gmi = nmengine.getModelInfo();
     let mi = gmi.modelInfo;
-    console.log("[tcModelAnalysis] used: %d, max ctx: %d, max cat: %d\n", mi.count, mi.max_context, mi.max_category);
+    console.log(
+      "[tcModelAnalysis] used: %d, max ctx: %d, max cat: %d\n",
+      mi.count,
+      mi.max_context,
+      mi.max_category
+    );
 
     let gms = nmengine.getModelStat(1, mi.max_category);
     let ms = gms.modelStat;
 
     // Get values from Buffer
     let bytes = (mi.max_category + 1) * uint16Array.BYTES_PER_ELEMENT;
-    let histoCat = uint16Array(ms.histo_cat.reinterpret(bytes))
-    let histoDeg = uint16Array(ms.histo_deg.reinterpret(bytes))
+    let histoCat = uint16Array(ms.histo_cat.reinterpret(bytes));
+    let histoDeg = uint16Array(ms.histo_deg.reinterpret(bytes));
 
     console.log("<tcModelAnalysis histoCat>");
     console.log(histoCat);
 
-
     let bytes2 = (mi.max_category + 1) * Uint16Array.BYTES_PER_ELEMENT;
-    let histoCat2 = new Uint16Array(ms.histo_cat.reinterpret(bytes2))
+    let histoCat2 = new Uint16Array(ms.histo_cat.reinterpret(bytes2));
 
     console.log("<msStr histo_cat deref3>");
     console.log(histoCat2);
@@ -291,11 +338,11 @@ ai[MODULES.NM500] = class extends ModuleBase {
 
     // Get values from Buffer
     let bytes3 = (mi.max_category + 1) * uint16Array.BYTES_PER_ELEMENT;
-    let histoCat3 = uint16Array(ms.histo_cat.reinterpret(bytes3))
+    let histoCat3 = uint16Array(ms.histo_cat.reinterpret(bytes3));
 
     console.log("<tcModelAnalysis histoCat>");
     console.log(histoCat);
-  }
+  };
 
   /**
    * 모듈 실행
@@ -444,6 +491,163 @@ ai[MODULES.DECISION_MAKER] = class extends ModuleBase {
       // properties 확인
       // console.log(`${this.getName()} 의 속성값을 확인합니다.`);
       // this.printProperty(this.getProperties());
+
+      // output 저장공간
+      var output1 = new ModuleData(DATA_TYPE.IMAGE, [
+        this.getID(),
+        this.getID(),
+        this.getID(),
+        this.getID(),
+      ]);
+
+      output = new ModuleDataChunk();
+      output.addModuleData(output1);
+
+      return RESULT_CODE.SUCCESS;
+    }
+  }
+};
+
+ai[MODULES.SCANNER] = class extends ModuleBase {
+  constructor() {
+    super();
+
+    // default properties
+    this.initialize({
+      ROI: {
+        type: PROP_TYPE.GROUP,
+        properties: {
+          Width: {
+            type: PROP_TYPE.NUMBER_EDIT,
+            value: 16,
+          },
+          Height: {
+            type: PROP_TYPE.NUMBER_EDIT,
+            value: 16,
+          },
+        },
+      },
+      Stride: {
+        type: PROP_TYPE.GROUP,
+        properties: {
+          x: {
+            type: PROP_TYPE.NUMBER_EDIT,
+            value: 16,
+          },
+          y: {
+            type: PROP_TYPE.NUMBER_EDIT,
+            value: 16,
+          },
+        },
+      },
+    });
+  }
+
+  /**
+   * 모듈 실행
+   * @param {List<ModuleDataChunk>} inputs
+   */
+  process(inputs) {
+    // 입력받아야되는 input의 개수
+    var mustInputSize = this.getParentIds().length;
+
+    console.log(
+      `[PL Process] ${this.getName()} (input: ${
+        inputs.length
+      }/${mustInputSize})`
+    );
+
+    // input data 찍어보기
+    // console.log(inputs);
+
+    var output;
+    if (mustInputSize !== inputs.length) {
+      console.log(
+        `${this.getName()} input이 모두 들어오지 않아 실행하지 않습니다.`
+      );
+      return null;
+    } else {
+      // process 시작
+
+      // merge는 아직 구현 X. 우선 ROI는 첫 번쨰 input만 사용하도록 구현한다.
+      let mergeInputData = inputs[0].getModuleDataList()[0].getData();
+
+      // Get properties
+      const props = this.getProperties();
+      const roiWidth = Number(
+        props.getIn(["ROI", "properties", "Width", "value"])
+      );
+      const roiHeight = Number(
+        props.getIn(["ROI", "properties", "Height", "value"])
+      );
+      const strideX = Number(
+        props.getIn(["Stride", "properties", "x", "value"])
+      );
+      const strideY = Number(
+        props.getIn(["Stride", "properties", "y", "value"])
+      );
+
+      // Calculate scanning count
+      const scanCountCol =
+        (mergeInputData.width - roiWidth) / strideX > 0
+          ? Math.floor((mergeInputData.width - roiWidth) / strideX + 1)
+          : 0;
+      const scanCountRow =
+        (mergeInputData.height - roiHeight) / strideY > 0
+          ? Math.floor((mergeInputData.height - roiHeight) / strideY + 1)
+          : 0;
+
+      let canvas = new OffscreenCanvas(
+        mergeInputData.width,
+        mergeInputData.height
+      );
+      let context = canvas.getContext("2d");
+      context.putImageData(mergeInputData, 0, 0);
+
+      // Start scanning
+      let dataList = [];
+      let x = 0;
+      let y = 0;
+
+      for (let row = 0; row < scanCountRow; row++) {
+        y = row * strideY;
+        for (let col = 0; col < scanCountCol; col++) {
+          x = col * strideX;
+          let roiData = context.getImageData(x, y, roiWidth, roiHeight);
+          console.log("[scanner] ", x, y, roiWidth, roiHeight);
+
+          // RGBA -> RGB (Alpha 제외)
+          let noAlpha = Array.from(
+            ImageFormatConverter.convertRGBAtoRGB(roiData.data)
+          );
+
+          dataList.push({ data: noAlpha, category: null });
+        }
+      }
+
+      // Classify
+      for (let i = 0; i < dataList.length; i++) {
+
+      // vector값 이용하여 classify 
+      let classify = nmengine.classify(1, dataList[i].data);
+
+      // Classify result
+      if (classify.resultCode === constants.SUCCESS) {
+        let req = classify.req;
+        let category = req.matched_count > 0 ? req.matched_count[0] : null;
+
+        // classify 결과 카테고리를 저장
+        dataList[i].category = category;
+        console.log(category);
+      } else {
+        console.log(
+          "[classify] Error: Failed to classify. %d\n",
+          classify.resultCode
+        );
+      }
+      }
+      
+      console.log(dataList);
 
       // output 저장공간
       var output1 = new ModuleData(DATA_TYPE.IMAGE, [
